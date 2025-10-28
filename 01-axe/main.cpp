6694dccdb6b573be669ffb64c96aa4f840d142bb
pedroni <pedronidegodois@hotmail.com>
2025-10-28 15:20:24 -0300

    init empty project

---
+++ 01-axe/main.cpp
@@ -0,0 +1,28 @@
+#include "raylib.h"
+
+int main() {
+  // Initialization
+  const int screenWidth = 800;
+  const int screenHeight = 450;
+
+  InitWindow(screenWidth, screenHeight, "01-axe Game");
+
+  SetTargetFPS(60);
+
+  // Main game loop
+  while (!WindowShouldClose()) {
+    // Update
+
+    // Draw
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Congrats! You created your first raylib window!", 190, 200, 20,
+             LIGHTGRAY);
+    EndDrawing();
+  }
+
+  // De-Initialization
+  CloseWindow();
+
+  return 0;
+}
