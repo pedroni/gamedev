d348a0a3d0531b46b1cc78a596882bfd4cf1f733
pedroni <pedronidegodois@hotmail.com>
2025-10-28 17:21:00 -0300

    axe game

--- 01-axe/main.cpp
+++ 01-axe/main.cpp
@@ -1,27 +1,103 @@
 #include "raylib.h"
+#include <iostream>
+#include <ostream>
+#include <sys/syslimits.h>
 
 int main() {
-  // Initialization
-  const int screenWidth = 800;
-  const int screenHeight = 450;
-
-  InitWindow(screenWidth, screenHeight, "01-axe Game");
+  const int screenWidth = 640;
+  const int screenHeight = 360;
+  InitWindow(screenWidth, screenHeight, "Axe Game");
 
   SetTargetFPS(60);
 
-  // Main game loop
-  while (!WindowShouldClose()) {
-    // Update
+  int circleRadius = 25;
+  int circleX = 0;
+  int circleY = 0;
+  int circleSpeed = 4;
+  // circle edges
+  int leftCircleX = circleX - circleRadius;
+  int rightCircleX = circleX + circleRadius;
+  int topCircleY = circleY - circleRadius;
+  int bottomCircleY = circleY + circleRadius;
 
-    // Draw
+  // rectangle
+  int rectSize = 80;
+
+  int rectX = (screenWidth - rectSize) / 2;
+  int rectY = 80;
+
+  int rectDirection = 10;
+
+  // rect edges
+  int leftRectX = rectX;
+  int rightRectX = rectX + rectSize;
+  int topRectY = rectY;
+  int bottomRectY = rectY + rectSize;
+
+  bool collided = false;
+
+  while (!WindowShouldClose()) {
     BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("Congrats! You created your first raylib window!", 190, 200, 20,
-             LIGHTGRAY);
+
+    if (!collided) {
+
+      // logic
+
+      leftCircleX = circleX - circleRadius;
+      rightCircleX = circleX + circleRadius;
+      topCircleY = circleY - circleRadius;
+      bottomCircleY = circleY + circleRadius;
+
+      leftRectX = rectX;
+      rightRectX = rectX + rectSize;
+      topRectY = rectY;
+      bottomRectY = rectY + rectSize;
+
+      circleSpeed = IsKeyDown(KEY_LEFT_SHIFT) ? 8 : 4;
+
+      if (IsKeyDown(KEY_A) && circleX > 0) {
+        circleX -= circleSpeed * 1;
+      }
+
+      if (IsKeyDown(KEY_D) && circleX < screenWidth) {
+        circleX += circleSpeed * 1;
+      }
+
+      if (IsKeyDown(KEY_W) && circleY > 0) {
+        circleY -= circleSpeed * 1;
+      }
+
+      if (IsKeyDown(KEY_S) && circleY < screenHeight) {
+        circleY += circleSpeed * 1;
+      }
+
+      // handle rect
+      rectY += rectDirection;
+      if (rectY > (screenHeight - rectSize) || rectY < 0) {
+        rectDirection = -rectDirection;
+      }
+
+      collided = topCircleY <= bottomRectY && bottomCircleY >= topRectY &&
+                 rightCircleX >= leftRectX && leftCircleX <= rightRectX;
+    }
+
+    ClearBackground(BLUE);
+    DrawCircle(circleX, circleY, circleRadius, WHITE);
+    DrawRectangle(rectX, rectY, rectSize, rectSize, BLACK);
+
+    if (collided) {
+      DrawText("GAME OVER", (screenWidth - 110) / 2, (screenHeight - 40) / 2,
+               20, RED);
+      if (IsKeyDown(KEY_SPACE)) {
+        collided = false;
+        circleX = 0;
+        circleY = 0;
+      }
+    }
+
     EndDrawing();
   }
 
-  // De-Initialization
   CloseWindow();
 
   return 0;
