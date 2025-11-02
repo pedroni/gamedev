8acec84ffa351dd9ede224c91f9b66989745cbbb
pedroni <pedronidegodois@hotmail.com>
2025-11-02 16:16:43 -0300

    timer & animation

---
+++ 04-shooter-platformer/src/animation.h
@@ -0,0 +1,21 @@
+#ifndef animation_h
+#define animation_h
+
+#include "timer.h"
+class Animation {
+    Timer timer;
+    int frameCount;
+
+  public:
+    Animation() : timer(Timer(0)), frameCount(0) {}
+    Animation(int frameCount, float duration) : timer(duration), frameCount(frameCount) {}
+
+    float getLength() const { return timer.getDuration(); }
+
+    int currentFrame() const {
+        return static_cast<int>(timer.getTime() / timer.getDuration() * frameCount);
+    }
+    void step(float deltaTime) { timer.step(deltaTime); }
+};
+
+#endif
---
+++ 04-shooter-platformer/src/timer.h
@@ -0,0 +1,29 @@
+#ifndef timer_h
+#define timer_h
+
+class Timer {
+    float duration, time;
+
+    bool timeout;
+
+  public:
+    Timer(float length) : duration(length), time(0) {}
+    void step(float deltaTime) {
+        time += deltaTime;
+
+        // here we could've reset the time to 0, but we don't want that. we might lose a
+        // few milliseconds or nanoseconds if we set to 0, this happens because our games
+        // runs concurrently and a small fraction of time could've passed
+        if (time >= duration) {
+            timeout = true;
+            time -= duration;
+        }
+    }
+
+    bool isTimeout() const { return timeout; }
+    float getTime() const { return time; }
+    float getDuration() const { return duration; };
+    void reset() { time = 0; }
+};
+
+#endif
